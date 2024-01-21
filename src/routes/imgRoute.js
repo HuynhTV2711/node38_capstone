7669aee11a27a2e4d6030f4bdf9d5dee6b244cf3
HUYNHTV2711 <huynhptit2711@gmail.com>
2024-01-21 09:59:46 +0700

    update authen, autho cho các API cần thiết

--- src/routes/imgRoute.js
+++ src/routes/imgRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { checkSave, getImg, getImgByIdUser } from "../controllers/imgController.js";
+import { khoaApi } from "../config/jwt.js";
 
 
 const imgRoute = express.Router();
 
 imgRoute.get("/get-img", getImg);
-imgRoute.get("/get-img/:idNguoiDung", getImgByIdUser)
+imgRoute.get("/get-img/:idNguoiDung", khoaApi, getImgByIdUser)
 imgRoute.get("/check-save/:idImg", checkSave)
 
 
